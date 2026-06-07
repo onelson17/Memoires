1530c3f85e6446b48aac535c2de1e582167902a1
onelson <thebantonn@gmail.com>
2026-06-07 19:55:27 +0200

    feat: navigation par onglet fonctionnelle + correction structure projet

--- js/router.js
+++ js/router.js
@@ -1,2 +1,20 @@
 const nav = document.querySelectorAll("nav li")
 const sections = document.querySelectorAll("section")
+
+
+export default function router() {
+    nav.forEach((li) => {
+        li.addEventListener("click", (event) => {
+            event.preventDefault()
+        nav.forEach((navItem) => {
+            navItem.classList.remove("active")
+        })
+        const target = document.getElementById(li.dataset.id)
+        sections.forEach((section) => {
+            section.classList.remove("active")
+        })
+        li.classList.add("active")
+        target.classList.add("active")
+    })
+})
+}
