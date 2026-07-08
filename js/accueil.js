b1ac1a3887944c6d32ff283a333f87c0b2d8d00f
onelson <thebantonn@gmail.com>
2026-07-08 21:33:23 +0200

    feat: validation formulaire - bouton actif selon conditions remplies

--- js/accueil.js
+++ js/accueil.js
@@ -16,8 +16,10 @@ livres.forEach((book) => {
 });
 pageX.value = "0"
 }
-
+// REMPLISSAGE DU DROP DOWN - AFFIACHE DE LA PROGRESSION DE LECTURE - DISABLED UNIQUEMENT SI LIVRE SELECTIONNÉ // 
 export function bookSelected() {
+    const title = document.getElementById("resume-title")
+    const texteArea = document.getElementById("text-zone")
     const livres = JSON.parse(localStorage.getItem("livres")) || []
     const selectBook = document.getElementById("books-drop")
     selectBook.addEventListener("change", (event) => {
@@ -26,13 +28,49 @@ export function bookSelected() {
             return book.id === Number(bookActual)
         })
         const pageX = document.getElementById("page-x")
-        pageX.value = selectedBook.progression
         if (bookActual !== "" ) {
-        const title = document.getElementById("resume-title")
-        const texteArea = document.getElementById("text-zone")
+        pageX.value = selectedBook.progression
         title.disabled = false
         texteArea.disabled = false
-    }
+
+        } else {
+
+        title.disabled = true
+        texteArea.disabled = true
+
+        }
     })
-    
+}
+
+// VALIDATION DU FORMULAIRE & CONDITIONS //
+
+export function submitTaskform() {
+
+    const taskForm = document.querySelector(".task-form")
+    const livre = document.getElementById("books-drop")
+    const pageX = document.getElementById("page-x")
+    const pageY = document.getElementById("page-y")
+    const title = document.getElementById("resume-title")
+    const zoneTexte = document.getElementById("text-zone")
+    function btnCheck() {
+        const valider = document.getElementById("btn-submit")
+        if (livre.value && pageY.value && title.value && zoneTexte.value.length >= 20) {
+            valider.disabled = false
+        } else {
+            valider.disabled = true
+        }}
+
+    taskForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+    if (!livre.value || !pageX.value || !pageY.value || !title.value || !zoneTexte.value.length >= 20) {
+        return
+    } 
+    })
+    taskForm.addEventListener("input", (event) => {
+        btnCheck()
+         })
+    livre.addEventListener("change", (event) => {
+        btnCheck()
+    })
+   
 }
--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ import router from "./router.js"
 import { ajoutLivres, afficherLivres } from "./parametres.js"
 import { alimenterDropdown } from "./accueil.js"
 import { bookSelected } from "./accueil.js"
+import { submitTaskform } from "./accueil.js"
 
 router()
 ajoutLivres()
 afficherLivres()
 alimenterDropdown()
 bookSelected()
+submitTaskform()
